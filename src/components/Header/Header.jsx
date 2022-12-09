df42652f62e757eb56615aaeaecb011b5572f207
nimakarimi76 <nimakarimi676@gmail.com>
2022-12-09 16:36:56 +0100

    feat: hamburger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import Logo from "../../assets/logo.png";
+import Bars from "../../assets/bars.png";
 
 const Header = () => {
+  let isMobile = window.innerWidth < 768 ? true : false;
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const showMobileMenu = () => {
+    if (isMobile)
+      return (
+        <img
+          onClick={() => setOpenMenu((prev) => !prev)}
+          className="bars"
+          src={Bars}
+          alt="hamburger menu"
+        />
+      );
+  };
+
   return (
     <div className="header">
       <img className="logo" src={Logo} alt="" />
-
-      <ul className="header-menu">
-        <li>Home</li>
-        <li>Programs</li>
-        <li>Why us</li>
-        <li>Plans</li>
-        <li>Testimonials</li>
-      </ul>
+      {showMobileMenu()}
+      {isMobile && !openMenu ? null : (
+        <ul
+          className="header-menu"
+          style={{ display: openMenu || !isMobile ? "flex" : "none" }}
+        >
+          <li onClick={() => setOpenMenu(false)}>Home</li>
+          <li onClick={() => setOpenMenu(false)}>Programs</li>
+          <li onClick={() => setOpenMenu(false)}>Why us</li>
+          <li onClick={() => setOpenMenu(false)}>Plans</li>
+          <li onClick={() => setOpenMenu(false)}>Testimonials</li>
+        </ul>
+      )}
     </div>
   );
 };
